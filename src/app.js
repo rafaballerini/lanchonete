6ecaff98c72e654258d527414a8b1389967700ee
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-03-11 11:11:48 -0300

    Vídeo 2.2

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const livros = [
   {id: 1, "titulo": "Senhor dos Aneis"},
   {id: 2, "titulo": "O Hobiit"}
@@ -13,6 +15,11 @@ app.get('/', (req, res) => {
 
 app.get('/livros', (req, res) => {
   res.status(200).json(livros)
+})
+
+app.post('/livros', (req, res) => {
+  livros.push(req.body);
+  res.status(201).send('Livro foi cadastrado com sucesso')
 })
 
 export default app
