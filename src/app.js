6c9d7b69e220fcf190cfc9649a8f54550f7cb0e4
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-03-11 11:28:30 -0300

    Vídeo 2.3

--- src/app.js
+++ src/app.js
@@ -17,9 +17,25 @@ app.get('/livros', (req, res) => {
   res.status(200).json(livros)
 })
 
+app.get('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  res.json(livros[index]);
+})
+
 app.post('/livros', (req, res) => {
   livros.push(req.body);
   res.status(201).send('Livro foi cadastrado com sucesso')
 })
+
+app.put('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+
+})
+
+function buscaLivro(id) {
+  return livros.findIndex(livro => livro.id == id)
+}
 
 export default app
