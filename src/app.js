b67d76b7030dcbcab7a3855f6e7e3bf29cffa0c7
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-03-11 15:14:33 -0300

    Vídeo 2.4

--- src/app.js
+++ src/app.js
@@ -31,8 +31,15 @@ app.put('/livros/:id', (req, res) => {
   let index = buscaLivro(req.params.id);
   livros[index].titulo = req.body.titulo;
   res.json(livros);
-
 })
+
+app.delete('/livros/:id', (req, res) => {
+  let {id} = req.params;
+  let index = buscaLivro(id);
+  livros.splice(index, 1);
+  res.send(`Livro ${id} removido com sucesso`);
+})
+
 
 function buscaLivro(id) {
   return livros.findIndex(livro => livro.id == id)
