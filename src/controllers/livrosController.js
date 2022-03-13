a8e82c0258299ad8d08b965f5d0aa39530c27836
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-03-13 20:42:52 -0300

    Vídeo 4.3

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -8,6 +8,18 @@ class LivroController {
   })
   }
 
+  static listarLivroPorId = (req, res) => {
+    const id = req.params.id;
+
+    livros.findById(id, (err, livros) => {
+      if(err) {
+        res.status(400).send({message: `${err.message} - Id do livro não localizado.`})
+      } else {
+        res.status(200).send(livros);
+      }
+    })
+  }
+
   static cadastrarLivro = (req, res) => {
     let livro = new livros(req.body);
 
@@ -17,6 +29,18 @@ class LivroController {
         res.status(500).send({message: `${err.message} - falha ao cadastrar livro.`})
       } else {
         res.status(201).send(livro.toJSON())
+      }
+    })
+  }
+
+  static atualizarLivro = (req, res) => {
+    const id = req.params.id;
+
+    livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+      if(!err) {
+        res.status(200).send({message: 'Livro atualizado com sucesso'})
+      } else {
+        res.status(500).send({message: err.message})
       }
     })
   }
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router
   .get("/livros", LivroController.listarLivros)
+  .get("/livros/:id", LivroController.listarLivroPorId)
   .post("/livros", LivroController.cadastrarLivro)
+  .put("/livros/:id", LivroController.atualizarLivro)
 
 export default router;   
