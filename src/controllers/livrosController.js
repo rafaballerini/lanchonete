cfdbf6ad983afa0eec8a9a3352b1487f27d060f8
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-03-13 22:41:18 -0300

    Vídeo 5.3

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -61,6 +61,17 @@ class LivroController {
     })
   }
 
+  static listarLivroPorEditora = (req, res) => {
+    const editora = req.query.editora
+
+    livros.find({'editora': editora}, {}, (err, livros) => {
+      res.status(200).send(livros);
+
+    })
+  }
+
+
+
 }
 
 export default LivroController
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router
   .get("/livros", LivroController.listarLivros)
+  .get("/livros/busca", LivroController.listarLivroPorEditora)
   .get("/livros/:id", LivroController.listarLivroPorId)
   .post("/livros", LivroController.cadastrarLivro)
   .put("/livros/:id", LivroController.atualizarLivro)
